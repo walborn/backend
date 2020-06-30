216c192b4f6e060ec14063ea1b1b846dd1e279f1
walborn <yuzhakov.boris@gmail.com>
2020-06-30 22:42:57 +0300

    remove body-parser cos express has the same functionality

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const compression = require('compression')
-const bodyParser = require('body-parser')
 
 
 const app = express()
 
 app.use(cors())
 app.use(compression())
-app.use(bodyParser.json())
+app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
 app.use('/auth', require('./routes/auth'))
 app.use('/user', require('./routes/user'))
 
