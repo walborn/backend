5b96295741d20c4ef0d1875e353c73b563b5433b
walborn <yuzhakov.boris@gmail.com>
2020-07-20 15:57:09 +0300

    fix dependabot alerts

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('../config')
 
 
 module.exports = [
-  expressJwt({ secret: config.access.secret }),
+  expressJwt({ secret: config.access.secret, algorithms: [ 'HS256' ] }),
   (err, req, res, next) => {
     if (err.name === 'UnauthorizedError') res.status(err.status).send(err)
     next()
